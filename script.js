61a718de516e372469973a31e9b4a451680b793f
Rajat <130392640+rajatcantcode@users.noreply.github.com>
2024-02-04 15:39:24 +0530

    2..Initial Starfall completed

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var c = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
+
 /*
  * ------------------------------------------
  * *-----------------------------
@@ -18,6 +20,9 @@ class Star {
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.velocity = { x: 0, y: 3 };
+    this.gravity = 1;
+    this.reducedAcceleration = 0.8;
   }
 
   draw() {
@@ -30,6 +35,17 @@ class Star {
 
   update() {
     this.draw();
+    //When ball hits the bottom of the screen
+    //We are adding the velocity.y becuase we want the ball to be fully visible but
+    //due to constant acceleration it some parts gets submerged into the ground
+    if (this.y + this.radius + this.velocity.y > canvas.height) {
+      //we will be decreaing the acceleration of the ball as they hit the bottom
+      this.velocity.y = -this.velocity.y * this.reducedAcceleration;
+    } else {
+      this.velocity.y += this.gravity;
+    }
+
+    this.y += this.velocity.y;
   }
 }
 
@@ -40,12 +56,10 @@ class Star {
  * *-----------------------------
  * ------------------------------------------
  */
-// Implementation
 let stars;
 function init() {
   stars = [];
-
-  for (let i = 0; i < 400; i++) {
+  for (let i = 0; i < 2; i++) {
     stars.push(new Star(canvas.width / 2, 30, 30, "blue"));
   }
 }
@@ -54,6 +68,9 @@ init();
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  stars.forEach((star) => {
+    star.update();
+  });
 }
 animate();
 
